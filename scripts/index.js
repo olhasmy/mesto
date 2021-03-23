0b676661c3e7c9bb9887bf14a226aeb0fd3114ec
unknown <g.o.n.z.o.m.e@yandex.ru>
2021-03-23 23:59:12 +0300

    Доработка

---
+++ scripts/index.js
@@ -0,0 +1,30 @@
+let popup = document.querySelector(".popup");
+let openPopupBtn = document.querySelector('.profile__edit-button');
+let closePopupBtns = document.querySelector('.form__close-icon');
+let popupOverlay = document.querySelector('.overlay');
+let formElement = document.querySelector('.input');
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_job');
+let profName = document.querySelector('.profile__name');
+let profJob = document.querySelector('.profile__job');
+
+function openPopup() {
+    popup.classList.add('popup_visible');
+}
+
+function closePopup() {
+    popup.classList.remove('popup_visible');
+}
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profName.textContent = nameInput.value;
+    profJob.textContent = jobInput.value;
+}
+
+openPopupBtn.addEventListener('click', openPopup);
+closePopupBtns.addEventListener('click', closePopup);
+popupOverlay.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', closePopup);
+
